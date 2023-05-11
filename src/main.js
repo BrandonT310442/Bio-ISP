6a267c1eeb4ad9f8c7d692209686f26c9482b3ec
Brandon Tai <btai@bayviewglen.ca>
2023-05-10 21:12:24 -0400

    navbar and taiilwind

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import '../src/assets/style.css'
+import router from './router'
 
-createApp(App).mount('#app')
+import App from './App.vue'
+createApp(App).use(router).mount('#app')
